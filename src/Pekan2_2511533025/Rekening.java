516ff36986dab414f73efb16d7852ec338f03aba
DikaSiNpc <dikagiowanda33@gmail.com>
2026-09-17 16:44:34 +0700

    PraktikumPBO_Pekan2_C_2511533025

---
+++ src/Pekan2_2511533025/Rekening.java
@@ -0,0 +1,62 @@
+package Pekan2_2511533025;
+import java.util.ArrayList;
+
+public class Rekening {
+    String nomorRekening;
+    String namaPemilik;
+    double saldo;
+    
+    // Implementasi Asosiasi (1-to-many): Satu rekening memiliki banyak transaksi
+    ArrayList<Transaksi> riwayatTransaksi;
+
+    public Rekening(String nomor, String nama, double saldoAwal) {
+        this.nomorRekening = nomor;
+        this.namaPemilik = nama;
+        this.saldo = saldoAwal;
+        
+        // Wajib menginisialisasi ArrayList di dalam constructor agar tidak NullPointerException
+        this.riwayatTransaksi = new ArrayList<>();
+        
+        System.out.println("Rekening atas nama " + namaPemilik + " berhasil dibuat.");
+    }
+
+    public void setorTunai(double nominal) {
+        if (nominal > 0) {
+            saldo += nominal;
+            
+            // Merekam riwayat (Pembuatan objek Transaksi di dalam method)
+            String idTrx = "TRX-S-" + System.currentTimeMillis();
+            Transaksi trxBaru = new Transaksi(idTrx, "Kredit", nominal);
+            riwayatTransaksi.add(trxBaru);
+            
+            System.out.println("Setor tunai Rp" + nominal + " berhasil. Saldo saat ini: Rp" + saldo);
+        } else {
+            System.out.println("Gagal: Nominal setor harus lebih dari 0!");
+        }
+    }
+
+    public void tarikTunai(double nominal) {
+        if (nominal < 10000) {
+            System.out.println("Transaksi Gagal : Minimal nominal penarikan 10.000");
+        } else if (nominal > saldo) {
+            System.out.println("Transaksi Gagal: Saldo tidak mencukupi. Saldo Anda: Rp" + saldo);
+        } else {
+            saldo -= nominal;
+            
+            // Merekam riwayat transaksi tarik tunai (Debit)
+            String idTrx = "TRX-T-" + System.currentTimeMillis();
+            Transaksi trxBaru = new Transaksi(idTrx, "Debit", nominal);
+            riwayatTransaksi.add(trxBaru);
+            
+            System.out.println("Tarik tunai Rp" + nominal + " berhasil. Saldo saat ini: Rp" + saldo);
+        }
+    }
+
+    public void cekInformasi() {
+        System.out.println("--- INFO REKENING ---");
+        System.out.println("No. Rekening : " + nomorRekening);
+        System.out.println("Nama Pemilik : " + namaPemilik);
+        System.out.println("Saldo Akhir  : Rp" + saldo);
+        System.out.println("---------------------");
+    }
+}
